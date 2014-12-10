763ff2c96ce6b094805402e94a9bb6d2fe43105e
Will <teck.geek@hotmail.com>
2014-12-10 15:17:48 -0700

    Create tableScanner.rb

---
+++ tableScanner.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+require 'mysql'
+
+begin
+	con = Mysql.new 'localhost','user1','enter'
+	puts con.get_server_info
+	rs = con.query 'SELECT VERSION()'
+	puts rs.fetch_row
+	
+rescue Mysql::Error => e
+	puts e.error
+	puts e.errno
+
+Dir.foreach('/path/to/dir') do |scan|
+	next if item == '.' or item == ".."
+	name =File.realpath(scan)
+	con.push(name)
+	owner =File.owned?(scan)
+	con.push(owner)
+	ext = File.extname(scan)
+	con.push(ext)
+	size = File.size(scan)
+	con.push(size)
+end
+
+ensure
+	con.close if con
+end
