b2e35e8ff556f941f940ee50718f6011d274da49
Will <teck.geek@hotmail.com>
2014-12-17 02:30:45 -0700

    udpate
    build checksum abilities add crc32 default is md5

--- tableScanner.rb
+++ tableScanner.rb
@@ -8,7 +8,7 @@ require 'socket'
 require 'sequel'
 require 'optparse'
 
-def default
+def paranoid
 	DB.create_table :scanner do
 		primary_key :id
 		String :name
@@ -22,22 +22,50 @@ def default
 		String :updated_at
 		String :created_at
 	end
+	scanner
 end
 
 def modify
 end
 
-def paranoid
+def default
+
+end
+
+def scanner
+	# Scan directory and add information to array for each file found
+	puts "Scanning for directory information"
+	Dir.foreach(dir) do |item|
+		next if item == '.' or item == ".."
+		if checksum == crc32
+			Digest::crc32.hexdigest(item)
+		else
+			Digest::MD5.hexdigest(item)
+		end
+		items.insert(:name => File.basename(item), 
+				:path => File.realpath(item),
+				#Gathers information on readablity of file via world readable, returns 3 digit callulation, will be looking for a better return value
+				:permission => File.world_readable?(item),
+				:ext => File.extname(item),
+				:size => File.size(item),
+				:hash => checksum,
+				:hostname => Socket.gethostname,
+				:type => File.ftype(item),
+				:updated_at => File.mtime(item),
+				:created_at => File.ctime(item))
+	end
 end
 
 
 # Grab current directory for scanning
-puts "Getting current directory"
 $dir = Pathname.new(Pathname.pwd())
 threadCount = Facter.processorcount
 
-#create new Mysql instance and use username and password if non found
-DB = Sequel.sqlite
+#Instanciate variables
+$DB = Sequel.sqlite
+$userDir = nil
+$checksum = md5
+$strategy = default
 
 #grab arg values and parse
 $options = {}
@@ -46,55 +74,44 @@ OptionParser.new do |opts|
 
 	opts.banner = "Usage: -d [dir] -h [md5, crc32], -m [default, update, paranoid]"
 
-	opts.on("-d", "" "") do |dir|
-		$userDir = dir
+	opts.on("-d", "" "") do |d|
+		$userDir = d
 	end
 
-	opts.on("-h", "md5", "Run md5 hash check") do |md5|
-		$checksum = md5
+	opts.on("-h", "md5", "Run md5 hash check") do |h|
+		$checksum = "md5"
 	end
 
-	opts.on("-h", "crc32", "Run crc32 hash check") do |crc32|
-		$checksum =crc32
+	opts.on("-h", "crc32", "Run crc32 hash check") do |h|
+		$checksum ="crc32"
 	end
 
-	opts.on("-m", "update", "Update current sqlite database.") do |update|
-		$strategy = update
+	opts.on("-m", "update", "Update current sqlite database.") do |m|
+		$strategy = "update"
 	end
 
-	opts.on("-m", "paranoid", "Rebuilding sqlite database.") do |paranoid|
-		$strategy = paranoid
+	opts.on("-m", "paranoid", "Rebuild sqlite database.") do |m|
+		$strategy = "paranoid"
 	end
 
 end.parse!
+
+if userDir != nil 
+	dir = userDir
+end
+
+if strategy == paranoid
+	default
+elsif  strategy == update
+	modify	
+else
+	default
+end
 
 if DB.all == false
 	sqlScanner
 end
 
-
-puts "Are you going to (1)create or (2)modify a database"
-$userInput = get.chomp
-
 posts = DB.from(:scanner)
 
-
-
 items = DB[:scanner]
-
-# Scan directory and add information to array for each file found
-puts "Scanning for directory information"
-Dir.foreach(dir) do |item|
-	next if item == '.' or item == ".."
-	items.insert(:name => File.basename(item), 
-				:path => File.realpath(item),
-				#Gathers information on readablity of file via world readable, returns 3 digit callulation, will be looking for a better return value
-				:permission => File.world_readable?(item),
-				:ext => File.extname(item),
-				:size => File.size(item),
-				:hash => Digest::MD5.hexdigest(item),
-				:hostname => Socket.gethostname,
-				:type => File.ftype(item),
-				:updated_at => File.mtime(item),
-				:created_at => File.ctime(item))
-end
