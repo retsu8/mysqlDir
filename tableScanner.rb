326f10606a0cc8082841a88749b17a0aa2d67806
Will <teck.geek@hotmail.com>
2014-12-19 01:46:03 -0700

    update
    working to get the bugs out and the sql correct.

--- tableScanner.rb
+++ tableScanner.rb
@@ -59,32 +59,12 @@ class XXHASH < Checksum
 end
 
 def modify
-$items = DB[:dirScanner]
+$items = DB[:dirScanner].order(:name)
 Dir.foreach(dir) do |item|
   next if item == '.' or item == ".."
-  selection = items.select path
-  if (File.realpath(item) == selection
-    selection = items.select size
-      if File.size(item) == selection)
-        selection = items.select host
-        if File.gethostname == selection
-          return true
-        end
-      end
-  else
-    items.update(:name => File.basename(item), 
-  	  :path => File.realpath(item),
-	  #Gathers information on readablity of file via world readable, returns 3 digit callulation, will be looking for a better return value
-	  :permission => File.world_readable?(item),
-	  :ext => File.extname(item),
-	  :size => File.size(item),
-	  :hash => Checksum.hash(item),
-	  :hostname => Socket.gethostname,
-	  :type => File.ftype(item),
-	  :updated_at => File.mtime(item),
-	  :created_at => File.ctime(item))			
-    end
-  end
+  selection = items.where(:path => item)
+  if (File.realpath(item) == items.where(:size => item)) & (File.size(item) == items.where(:size => size)) & (File.gethostname == items.where(:host => File.gethostname))
+  else (update)
 end
 
 def default
@@ -113,7 +93,6 @@ end
 def scanner
 # Scan directory and add information to array for each file found
 puts "Scanning for directory information"
-puts "Hashing in " hash "."
 $items = DB[:dirScanner]
 Dir.foreach(dir) do |item|
 	next if item == '.' or item == ".."
@@ -128,8 +107,23 @@ Dir.foreach(dir) do |item|
 		:type => File.ftype(item),
 		:updated_at => File.mtime(item),
 		:created_at => File.ctime(item))
+	end
 end
+
+def update
+    #line 78 Gathers information on readablity of file via world readable, returns 3 digit callulation, will be looking for a better return value
+    items.update(:name => File.basename(item))
+    items.update(:path => File.realpath(item))
+    items.update(:permission => File.world_readable?(item))
+    items.update(:ext => File.extname(item))
+    items.update(:size => File.size(item))
+    items.update(:hash => Checksum.hash(item))
+    items.update(:hostname => Socket.gethostname)
+    items.update(:type => File.ftype(item))
+    items.update(:updated_at => File.mtime(item))
+    items.update(:created_at => File.ctime(item))	
 end
+
 
 #grab start time
 t1 = Time.now
@@ -142,7 +136,7 @@ $threadCount = Facter.processorcount
 $DB = Sequel.sqlite('dirScanner.db')
 $userDir = nil
 $hash = MD5
-$strategy = default
+$strategy = "default"
 
 #grab arg values and parse
 $options = {}
@@ -151,45 +145,45 @@ OptionParser.new do |opts|
 
 opts.banner = "Usage: -d [dir] -h [md5, crc32, sha256, sha384, sha512, ripem160, xxhash], -p [paranoid mode]"
 
-opts.on("-d", ".", "Get the directory from the user") do |d|
-	$userDir = d
-end
+  opts.on("-d", ".", "Get the directory from the user") do |d|
+    $userDir = d
+  end
 
-opts.on("-h", "md5", "Run md5 hash check") do |h|
-	$hash = MD5
-end
+  opts.on("-h", "md5", "Run md5 hash check") do |h|
+    $hash = MD5
+  end
 
-opts.on("-h", "crc32", "Run crc32 hash check") do |h|
-	$hash = CRC32
-end
+  opts.on("-h", "crc32", "Run crc32 hash check") do |h|
+    $hash = CRC32
+  end
 
-opts.on("-h", "sha256", "Run crc32 hash check") do |h|
-	$hash = SHA1
-end
+  opts.on("-h", "sha256", "Run crc32 hash check") do |h|
+    $hash = SHA1
+  end
 
-opts.on("-h", "sha384", "Run crc32 hash check") do |h|
-	$hash = SHA2
-end
+  opts.on("-h", "sha384", "Run crc32 hash check") do |h|
+    $hash = SHA2
+  end
 
-opts.on("-h", "sha512", "Run crc32 hash check") do |h|
-	$hash = SHA3
-end
+  opts.on("-h", "sha512", "Run crc32 hash check") do |h|
+    $hash = SHA3
+  end
 
-opts.on("-h", "ripemd160", "Run crc32 hash check") do |h|
+  opts.on("-h", "ripemd160", "Run crc32 hash check") do |h|
 	$hash = RIPEMD160
-end
+  end
 
-opts.on("-h", "xxhash", "Run crc32 hash check") do |h|
+  opts.on("-h", "xxhash", "Run crc32 hash check") do |h|
 	$hash = XXHASH
-end
+  end
 
-opts.on("-p", "Rebuild sqlite database.") do |m|
+  opts.on("-p", "Rebuild sqlite database.") do |m|
 	$strategy = "paranoid"
-end
+  end
 
-opts.on("-v", "--verbose", "verbose mode") do
+  opts.on("-v", "--verbose", "verbose mode") do
 	 @options[:verbose] = true
-end
+  end
 
 end.parse!
 
@@ -199,13 +193,13 @@ dir = userDir
 end
 
 #pick strategy to use
-if strategy == paranoid
-paranoid
-else
-default
-end
+if strategy == "paranoid" 
+	paranoid
+else 
+	default
 
 #grab end time
 t2 = Time.now
+t3 = t2-t1
 
-puts t2-t1
+puts t3.to_i
